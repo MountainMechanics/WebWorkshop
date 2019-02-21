ee38036d268131f14036cf50f586d39cbb10ec16
Felix Berger <6133@htl.rennweg.at>
2019-02-21 21:25:23 +0100

    [V: 0.1.1] First design with CSS grid responsive navbar design
    TODO:
    redo website and change to CSS grid

---
+++ subSites/mainSite.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mainsite</title>
+    <link rel="stylesheet" href="../CSS/basicStyle.css">
+    <link rel="stylesheet" href="../CSS/mainStyle.css">
+</head>
+<body>
+    <section class="box">
+        <h1>Welcome to the Mountain</h1>
+        <nav>
+            <label for="toggle">&#9776;</label>
+            <input type="checkbox" id="toggle"/>
+            <div class="menu">
+                <a href="#">My Skills</a>
+                <a href="#">Running Projects</a>
+                <a href="#">This Website</a>
+                <a href="#" id="contact">Contacts</a>
+            </div>
+        </nav>
+        <div id="content">
+            <header>
+                header
+            </header>
+            <article>
+                aricle
+            </article>
+            <main>
+                Main
+            </main>
+            <aside>
+                aside
+            </aside>
+            <footer>
+                footer
+            </footer>
+        </div>
+    </section>
+</body>
+</html>
