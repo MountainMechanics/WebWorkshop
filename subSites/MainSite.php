02a528d05a99c19b39f6c4cb4dea9edd722d6dc6
Felix Berger <6133@htl.rennweg.at>
2019-02-21 17:58:23 +0100

    [V: 0.1.1] Switched to a setup with xampp to make site to site linking easy and to make access and setup easy.

---
+++ subSites/MainSite.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mainsite</title>
+    <link rel="stylesheet" href="../CSS/basicStyle.css">
+    <link rel="stylesheet" href="../CSS/mainStyle.css">
+</head>
+<body>
+    <nav>Navbar</nav><br>
+    <section class="box">Here is going to be all the content</section>
+</body>
+</html>
